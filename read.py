5dad0628421279e60e0360280832776c05bf589e
Peggy-Pei <peggyguo@Peggyde-MacBook-Air.local>
2021-06-12 18:44:08 +0800

    filter

--- read.py
+++ read.py
@@ -16,3 +16,10 @@ for d in data:
     sum_len += len(d)
     ave = sum_len / len(data)
 print('留言的平均長度為', ave, '個字') 
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[1])
